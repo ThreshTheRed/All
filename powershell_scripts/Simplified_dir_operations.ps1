2a7b0da4bd7007bc000a509d74c580277b4ab00a
ThreshTheRed <ThreshTheRed@users.noreply.github.com>
2019-10-01 12:32:10 +0200

    Create Simplified_dir_operations.ps1
    Pack of functions for automation

---
+++ powershell_scripts/Simplified_dir_operations.ps1
@@ -0,0 +1,25 @@
+﻿function CreateFolder
+{
+    $dir_name = Read-Host
+    if(Test-Path -Path $dir_name)
+    { 
+        Write-Host "Directory already exised"
+    }   
+    else    
+    {
+        New-Item -Path $dir_name -ItemType "directory"
+    }
+}
+
+function DeleteFolder
+{
+    $dir_name = Read-Host
+    if(-not(Test-Path -Path $dir_name))
+    { 
+        Write-Host "Directory does not exist."
+    }
+    else    
+    {
+        Remove-Item -Path $dir_name
+    }
+}
