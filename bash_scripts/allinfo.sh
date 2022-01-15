26b6d78c858931893fa132a6ea1106d6fcda1cac
Daniel Ziemniak <38981736+ThreshTheRed@users.noreply.github.com>
2022-01-15 20:14:03 +0100

    Update of allinfo.sh script

--- bash_scripts/allinfo.sh
+++ bash_scripts/allinfo.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-# List total info about your system
 
-echo "Just enter the number"
-PS3='What do you wanna know? ' 
-options=("System info" "Hardware info" "CPU info" "Disk usage" "Quit")
+echo "It's better to run this script with sudo - some of the options may not work correctly without elevated permission"
+echo "Enter the number"
+PS3='What do you wanna know about this hardware? '
+options=("System info" "Hardware info" "CPU Name" "Disk model" "Disk usage" "Quit")
 select opt in "${options[@]}"
 do
-	case $opt in
-		"System info")
-			echo "`uname -a`"
-			;;
+        case $opt in
+                "System info")
+                        echo "$(grep -i 'version=' /etc/*-release | cut -d = -f2-)"
+                        ;;
 
-		"Hardware info")
-			echo "`lshw -short`"
-			;;
+                "Hardware info")
+                        echo "`lshw -short`"
+                        ;;
 
-		"CPU info"):
-			echo "`lscpu`"
-			;;
-		
-		"Disk usage"):
-			echo "`df -H`"
-			;;
+                "CPU Name"):
+                        echo "$(grep 'model name' /proc/cpuinfo -m 1 | cut -d" " -f3-)"
+                        ;;
 
-		"Quit")
-			break
-			;;
-	
-		*) 
-		echo "Invalid option $REPLY";;	
-	esac
+                "Disk model"):
+                        echo "$(fdisk -l | grep 'model' | cut -d " " -f4-)"
+                        ;;
+
+                "Disk usage"):
+                        echo "`df -H`"
+                        ;;
+
+                "Quit")
+                        break
+                        ;;
+
+                *)
+                echo "Invalid option $REPLY";;
+        esac
 done
-
-		
-
-
-
