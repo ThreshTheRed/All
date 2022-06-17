cd35e70e72b3e6945081dd5c8d87ea072cbb3666
DannyN <daniel.ziemniak@gmail.com>
2022-06-17 20:34:38 +0200

    Updated with 2do list

---
+++ bash_scripts/wireguard_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+	then printf "You have to run this command as a root\n"
+	exit
+else
+	printf "Installing wireguard"
+	apt install wireguard
+	mkdir -m 0700 /etc/wireguard_test/
+	#umask 077; wg genkey | tee privatekey | wg pubkey > publickey
+fi	
+
+
+# 2do in future
+# align it to check distro and act accordingly
+# custom names
+# QR code for a config?
