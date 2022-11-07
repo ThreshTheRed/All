543ac3a14de36b31a0a80a1af3dc13f349f2418a
Daniel Ziemniak <38981736+ThreshTheRed@users.noreply.github.com>
2022-11-07 14:47:44 +0100

    added powershell script
    script will change computer's description

---
+++ powershell_scripts/Change_description.ps1
@@ -0,0 +1,9 @@
+﻿[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = "Computer's description"
+$desc   = "Enter user's full name (ex. John Doe):"
+
+$text = [Microsoft.VisualBasic.Interaction]::InputBox($desc, $title)
+ 
+#$desc = Read-Host "Enter computer's description (full name of user) "
+Set-ADComputer -Identity $env:computername -Description $text
