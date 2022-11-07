a7594541c8be350ed72940b4eb16d4acf0b095e7
Daniel Ziemniak <38981736+ThreshTheRed@users.noreply.github.com>
2022-11-07 15:51:15 +0100

    Add files via upload
    script to change computer name using ADSI, instead of Get-AD (this module has to be installed separately)

---
+++ powershell_scripts/Change_description_ADSI.ps1
@@ -0,0 +1,14 @@
+﻿$UserName = $env:COMUTERNAME 
+$filter = "(&(objectCategory=computer)(objectClass=computer)(cn=$env:COMPUTERNAME))"
+$DN = ([adsisearcher]$filter).FindOne().Properties.distinguishedname
+$ObjClient = [ADSI]"LDAP://$DN"
+
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = "Computer's description"
+$desc   = "Enter user's full name (ex. John Doe):"
+
+$text = [Microsoft.VisualBasic.Interaction]::InputBox($desc, $title) 
+
+$ObjClient.Description = $text
+$ObjClient.SetInfo()
