9b987e6212eddd0f0b953a79c68f4205c853d429
DannyN <daniel.ziemniak@gmail.com>
2022-11-16 23:46:23 +0100

    fixed prvkey var & ip address
    Moved prvkey variable into if...else conditional and asked user for peer IP address

--- bash_scripts/wg_ccg.sh
+++ bash_scripts/wg_ccg.sh
@@ -2,8 +2,6 @@
 
 # This is the script to generate a simple wireguard config to access the server.
 
-prvkey=$(cat private.key)
-
 if (( $EUID != 0 ))
 	then printf "This cript can only be run as root..\n"
 	exit
@@ -11,7 +9,10 @@ else
 	printf "Generating peer config.\n"
 	printf "Provide name of config:\n"
 	read conf_name \n
+	printf "Provide IP address of a peer:\n"
+	read ip_addr
 	touch $conf_name.conf
 	wg genkey | tee private.key | wg pubkey | tee public.key >/dev/null 2>&1
-	printf "[Interface]\nAddress = 10.0.0.3/24\nPrivateKey = $prvkey" > $conf_name.conf
+	prvkey=$(cat private.key)
+	printf "[Interface]\nAddress = $ip_addr\nPrivateKey = $prvkey" > $conf_name.conf
 fi
