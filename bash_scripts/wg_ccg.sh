fc3cc8bedc9f1f9c86ef8cf723c14c82431edd39
DannyN <daniel.ziemniak@gmail.com>
2022-11-17 00:12:06 +0100

    Update wg_ccg.sh

--- bash_scripts/wg_ccg.sh
+++ bash_scripts/wg_ccg.sh
@@ -11,8 +11,11 @@ else
 	read conf_name \n
 	printf "Provide IP address of a peer:\n"
 	read ip_addr
+	printf "Provide server's public key:\n"
+	read srv_pub
+	printf "Provide server's public IP:\n"
 	touch $conf_name.conf
 	wg genkey | tee private.key | wg pubkey | tee public.key >/dev/null 2>&1
 	prvkey=$(cat private.key)
-	printf "[Interface]\nAddress = $ip_addr\nPrivateKey = $prvkey" > $conf_name.conf
+	printf "[Interface]\nAddress = $ip_addr\nPrivateKey = $prvkey\n[Peer]\nPublicKey = $srv_pub\nEndpoint = " > $conf_name.conf
 fi
