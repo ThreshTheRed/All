a6384fc5d5a79c7d5d9b52f3c22a53a7fd4765ae
Daniel Ziemniak <daniel.ziemniak@gmail.com>
2022-11-17 15:11:32 +0100

    added foreach loop & todo section

--- powershell_scripts/wg_ccg.ps1
+++ powershell_scripts/wg_ccg.ps1
@@ -1,3 +1,18 @@
 $conf_name = Read-Host -Prompt 'Input config name: '
+$full_cnf_name = "$conf_name.conf"
+$peer_ip = Read-Host -Prompt 'Input IP Address of a peer: '
+$srv_pubkey = Read-Host -Prompt 'Input public key of a server: '
+$srv_pubip = Read-Host -Prompt 'Input public IP address of a server: '
+New-Item -Path "." -Name "$full_cnf_name" -ItemType "file"
 
-New-Item -Path "c:\" -Name $conf_name'.conf' -ItemType "file"
+$lines = @("# Client section","[Peer]","PublicKey = ","AllowedIPs = "," ","# Server section","[Peer]","PublicKey = ","AllowedIPs = ","Endpoint = $srv_pubip")
+
+foreach ($i in $lines) {
+    Add-Content $full_cnf_name $i
+}
+
+#
+# to do
+# create private & public key
+# forward their content into correct variables
+#
